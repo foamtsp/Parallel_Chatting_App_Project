4e64264f166eb163a4fbaba00320e6194b0640f3
dolphinep <ping.waragon@gmail.com>
2020-04-07 01:27:49 +0700

    add some group api

--- server/Controller/groupController.js
+++ server/Controller/groupController.js
@@ -1,15 +1,46 @@
 const Group = require('../models/groupModel');
 
 //createGroup
+const createGroup = (req, res) => {
+    const group = new Group(req.body) //member and message is relation to schema what to do??
+    group.save(function (err, obj) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        else {
+            res.json({ message: 'Create Group Success', data: obj });
+        }
+    })
+    res.status(201).end()
+}
 
 //getAllGroup
+const getAllGroup = (req,res) => {
+    try {
+        const groups = Group.find({})
+        res.json(groups)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
 
 //addMember
 
-//getMember
+//getAllMember
+const getAllGroup = (req,res) => {
+    try {
+        const memers = Group.find({members})
+        res.json(memers)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
 
 //updateMember
 
 //getMessage
 
 //updateMessage
+
+module.exports = {createGroup,getAllGroup}
--- server/router.js
+++ server/router.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const userController = require('./Controller/userController');
-
+const groupController = require('./Controller/groupController');
 
 router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
 });
 
-router.post("/createuser", controller.createUser)
-router.get("/getuser/:name", controller.getUser)
-router.put("/update/:name", controller.updateStatus)
-router.put("/joingroup/:name", controller.joinGroup)
+//user
+router.post("/createuser", userController.createUser)
+router.get("/getuser/:name", userController.getUser)
+router.put("/update/:name", userController.updateStatus)
+router.put("/joingroup/:name", userController.joinGroup)
+
+//group
+router.post("/creategroup", groupController.createGroup)
+router.get("/groups", groupController.getAllGroup)
 
 module.exports = router;
