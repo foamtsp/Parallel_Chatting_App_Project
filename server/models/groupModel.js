176fc0670cd4b4523926d4c9b928af9adc354db8
Wisarut Tiyamanee <jtiyamanee@hotmail.com>
2020-04-06 19:51:39 +0700

    add groupModel relation

--- server/models/groupModel.js
+++ server/models/groupModel.js
@@ -6,13 +6,14 @@ const groupSchema = new mongoose.Schema({
         required: [true, 'Please fill the group name.'],
         unique: true
     },
-    members: { // rel to user Object
-        type: [String],
-        required: [true, 'Group must have at least one member.']
-    },
-    messages: { // rel to message Object
-        type: [String]
-    }
+    members: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    }],
+    messages: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Message'
+    }]
 });
 
 const Group = mongoose.model('Group', groupSchema);
