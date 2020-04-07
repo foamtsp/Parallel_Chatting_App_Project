7cb2473f4c4a8f2996d16d7d6b0f00d57a832f24
dolphinep <ping.waragon@gmail.com>
2020-04-07 17:55:38 +0700

    delete group

--- server/Controller/groupController.js
+++ server/Controller/groupController.js
@@ -5,18 +5,18 @@ const Message = require('../models/messageModel');
 
 // Get all group
 exports.getAllGroups = async (req, res, next) => {
-  try {
-    const groups = await Group.find();
+    try {
+        const groups = await Group.find();
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        groups,
-      },
-    });
-  } catch (err) {
-    throw new Error(err.message);
-  }
+        res.status(200).json({
+            status: 'success',
+            data: {
+                groups,
+            },
+        });
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
 
 // JSON format of createGroup:
@@ -25,105 +25,137 @@ exports.getAllGroups = async (req, res, next) => {
 //   groupName: "Group Name"
 // };
 exports.createGroup = async (req, res, next) => {
-  try {
-    const { name, groupName } = req.body;
+    try {
+        const { name, groupName } = req.body;
 
-    if (!name) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'A Group must have an founder.',
-      });
-      throw new Error('A Group must have an founder.');
+        if (!name) {
+            res.status(400).json({
+                status: 'fail',
+                message: 'A Group must have an founder.',
+            });
+            throw new Error('A Group must have an founder.');
+        }
+
+        const user = await User.findOneAndUpdate(
+            { name },
+            {
+                currentGroup: groupName,
+            }
+        );
+
+        if (!user) {
+            res.status(404).json({
+                status: 'fail',
+                message: 'Not found this user name.',
+            });
+            throw new Error('Not found this user name.');
+        }
+
+        const group = await Group.create({
+            groupName,
+            $push: { members: user._id },
+        });
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                group,
+            },
+        });
+    } catch (err) {
+        throw new Error(err.message);
     }
-
-    const user = await User.findOneAndUpdate(
-      { name },
-      {
-        currentGroup: groupName,
-      }
-    );
-
-    if (!user) {
-      res.status(404).json({
-        status: 'fail',
-        message: 'Not found this user name.',
-      });
-      throw new Error('Not found this user name.');
-    }
-
-    const group = await Group.create({
-      groupName,
-      $push: { members: user._id },
-    });
-
-    res.status(201).json({
-      status: 'success',
-      data: {
-        group,
-      },
-    });
-  } catch (err) {
-    throw new Error(err.message);
-  }
 };
 
 // Get a group with groupName
 exports.getGroup = async (req, res, next) => {
-  const name = req.params.name;
+    const name = req.params.name;
 
-  const group = await Group.findOne({ GroupName: name });
+    const group = await Group.findOne({ GroupName: name });
 
-  if (!group) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Not found a group with that name.',
+    if (!group) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Not found a group with that name.',
+        });
+        throw new Error('Not found a group with that name.');
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            group,
+        },
     });
-    throw new Error('Not found a group with that name.');
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      group,
-    },
-  });
 };
 
 // Edit group with name: EX. edit groupName NOT kick the member because it has trouble relations!!
 exports.updateGroup = async (req, res, next) => {
-  const name = req.params.name;
-  // ONLY edit groupName
-  const newGroupName = req.body.groupName;
+    const name = req.params.name;
+    // ONLY edit groupName
+    const newGroupName = req.body.groupName;
 
-  const group = await Group.findOneAndUpdate(
-    { GroupName: name },
-    {
-      groupName: newGroupName,
+    const group = await Group.findOneAndUpdate(
+        { GroupName: name },
+        {
+            groupName: newGroupName,
+        }
+    );
+
+    if (!group) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Not found a group with that name.',
+        });
+        throw new Error('Not found a group with that name.');
     }
-  );
 
-  if (!group) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Not found a group with that name.',
+    res.status(200).json({
+        status: 'success',
+        data: {
+            group,
+        },
     });
-    throw new Error('Not found a group with that name.');
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      group,
-    },
-  });
 };
 
 // NOT yet implement
+// pass by params or body????
 exports.deleteGroup = async (req, res, next) => {
-  // 1) Delete all messages in group (Create local function)
-  // 2) Delete all members in groupModel
-  // 3.1) Delete all dependencies with groupName in userRecord
-  // 3.2) Delete currentGroup with this groupName userModel (Create local function)
-  // 4) Delete this group
-  res.status(200).json({});
+    const { groupName } = req.params
+
+    const group = await Group.findOne({ groupName: groupName })
+    if (!group) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Not found a group with that name.',
+        });
+        throw new Error('Not found a group with that name.');
+    }
+
+    // 1) Delete all messages in group (Create local function)
+    const messages = group.messages
+    messages.map((_id) => {
+        await Message.findByIdAndRemove(_id)
+    })
+
+    // 2) Delete all members in groupModel + 3.2) Delete currentGroup with this groupName userModel (Create local function)
+    const members = group.members
+    members.map((_id) => {
+        const result = await User.findByIdAndUpdate(_id, {
+            $pull: { groupName: groupName }
+        })
+        if (result) {
+            console.log("Deleting member ", result)
+        }
+    })
+
+    // 3.1) Delete all dependencies with groupName in userRecord
+    await UserRecord.findOneAndRemove({ groupName: groupName })
+
+    // 4) Delete this group
+    await Group.deleteOne({ groupName: groupName })
+    
+    res.status(200).json({
+        status:'success'
+    });
 };
