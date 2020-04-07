8fcb847a06788f16942c140a2c545fd379fa13e5
Wisarut Tiyamanee <jtiyamanee@hotmail.com>
2020-04-07 18:11:04 +0700

    Auto stash before merge of "backend" and "origin/backend"

--- server/Controller/groupController.js
+++ server/Controller/groupController.js
@@ -5,18 +5,18 @@ const Message = require('../models/messageModel');
 
 // Get all group
 exports.getAllGroups = async (req, res, next) => {
-    try {
-        const groups = await Group.find();
+  try {
+    const groups = await Group.find();
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                groups,
-            },
-        });
-    } catch (err) {
-        throw new Error(err.message);
-    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        groups,
+      },
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 // JSON format of createGroup:
@@ -25,136 +25,172 @@ exports.getAllGroups = async (req, res, next) => {
 //   groupName: "Group Name"
 // };
 exports.createGroup = async (req, res, next) => {
-    try {
-        const { name, groupName } = req.body;
+  try {
+    const {
+      name,
+      groupName
+    } = req.body;
 
-        if (!name) {
-            res.status(400).json({
-                status: 'fail',
-                message: 'A Group must have an founder.',
-            });
-            throw new Error('A Group must have an founder.');
-        }
-
-        const user = await User.findOneAndUpdate(
-            { name },
-            {
-                currentGroup: groupName,
-            }
-        );
-
-        if (!user) {
-            res.status(404).json({
-                status: 'fail',
-                message: 'Not found this user name.',
-            });
-            throw new Error('Not found this user name.');
-        }
-
-        const group = await Group.create({
-            groupName,
-            $push: { members: user._id },
-        });
-
-        res.status(201).json({
-            status: 'success',
-            data: {
-                group,
-            },
-        });
-    } catch (err) {
-        throw new Error(err.message);
+    if (!name) {
+      res.status(400).json({
+        status: 'fail',
+        message: 'A Group must have an founder.',
+      });
+      throw new Error('A Group must have an founder.');
     }
+
+    const user = await User.findOneAndUpdate({
+      name
+    }, {
+      currentGroup: groupName,
+    });
+
+    if (!user) {
+      res.status(404).json({
+        status: 'fail',
+        message: 'Not found this user name.',
+      });
+      throw new Error('Not found this user name.');
+    }
+
+    const group = await Group.create({
+      groupName,
+      $push: {
+        members: user._id
+      },
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        group,
+      },
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 // Get a group with groupName
 exports.getGroup = async (req, res, next) => {
+  try {
     const name = req.params.name;
 
-    const group = await Group.findOne({ groupName: name });
+    const group = await Group.findOne({
+      groupName: name
+    });
 
     if (!group) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'Not found a group with that name.',
-        });
-        throw new Error('Not found a group with that name.');
+      res.status(404).json({
+        status: 'fail',
+        message: 'Not found a group with that name.',
+      });
+      throw new Error('Not found a group with that name.');
     }
 
     res.status(200).json({
-        status: 'success',
-        data: {
-            group,
-        },
+      status: 'success',
+      data: {
+        group,
+      },
     });
+
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 // Edit group with name: EX. edit groupName NOT kick the member because it has trouble relations!!
 exports.updateGroup = async (req, res, next) => {
+
+  try {
     const name = req.params.name;
     // ONLY edit groupName
     const newGroupName = req.body.groupName;
 
-    const group = await Group.findOneAndUpdate(
-        { groupName: name },
-        {
-            groupName: newGroupName,
-        }
-    );
+    const group = await Group.findOneAndUpdate({
+      groupName: name
+    }, {
+      groupName: newGroupName,
+    });
 
     if (!group) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'Not found a group with that name.',
-        });
-        throw new Error('Not found a group with that name.');
+      res.status(404).json({
+        status: 'fail',
+        message: 'Not found a group with that name.',
+      });
+      throw new Error('Not found a group with that name.');
     }
 
     res.status(200).json({
-        status: 'success',
-        data: {
-            group,
-        },
+      status: 'success',
+      data: {
+        group,
+      },
     });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+
 };
 
 // NOT yet implement
 // pass by params or body????
 exports.deleteGroup = async (req, res, next) => {
-    const { groupName } = req.params
+  try {
+    const {
+      groupName
+    } = req.params
 
-    const group = await Group.findOne({ groupName: groupName })
+    const group = await Group.findOne({
+      groupName: groupName
+    })
     if (!group) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'Not found a group with that name.',
-        });
-        throw new Error('Not found a group with that name.');
-    }
+      res.status(404).json({
+        status: 'fail',
+        message: 'Not found a group with that name.',
+      });
+      throw new Error('Not found a group with that name.');
+    };
 
     // 1) Delete all messages in group (Create local function)
     const messages = group.messages
     messages.map((_id) => {
+<<<<<<< Updated upstream
         try {
             await Message.findByIdAndRemove(_id)
         } catch (error) {
             throw new Error(error.message);
         }
+=======
+      await Message.findByIdAndRemove(_id)
+>>>>>>> Stashed changes
     })
 
     // 2) Delete all members in groupModel + 3.2) Delete currentGroup with this groupName userModel (Create local function)
     const members = group.members
     members.map((_id) => {
+<<<<<<< Updated upstream
         try {
             await User.findByIdAndUpdate(_id, {
                 $pull: { groupName: groupName }
             })
         } catch (error) {
             throw new Error(error.message);
+=======
+      const result = await User.findByIdAndUpdate(_id, {
+        $pull: {
+          groupName: groupName
+>>>>>>> Stashed changes
         }
-    })
+      })
+      if (result) {
+        console.log("Deleting member ", result)
+      }
+    });
 
     // 3.1) Delete all dependencies with groupName in userRecord
+<<<<<<< Updated upstream
     try {
         await UserRecord.findOneAndRemove({ groupName: groupName })
     } catch (error) {
@@ -171,5 +207,21 @@ exports.deleteGroup = async (req, res, next) => {
 
     res.status(200).json({
         status: 'success'
+=======
+    await UserRecord.findOneAndRemove({
+      groupName: groupName
+    })
+
+    // 4) Delete this group
+    await Group.deleteOne({
+      groupName: groupName
+    })
+
+    res.status(200).json({
+      status: 'success'
+>>>>>>> Stashed changes
     });
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
