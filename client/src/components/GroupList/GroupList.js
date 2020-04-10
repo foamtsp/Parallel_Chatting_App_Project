50a29142bd67918189a2798f20806684edeb3434
drive087 <d.thus_sk135@hotmail.com>
2020-04-10 22:02:35 +0700

    add join leave group

--- client/src/components/GroupList/GroupList.js
+++ client/src/components/GroupList/GroupList.js
@@ -1,24 +1,87 @@
 import React, { useState, useEffect } from "react";
 import './GroupList.css'
 
-const onCreateGroup = (name,group) => {
+const onChangeGroupChat = (name,group) => {
+
   window.location.href = "/chat?name="+name+'&room='+group;
 }
 
+const joinGroup = (name,groupName) => {
 
+  var timer = null;
+  
+  var sending_data = {
+    groupName:groupName,
+  }
+
+  fetch("http://localhost:4000/api/users/"+name+"/joingroup" , {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(sending_data)
+})
+    .then(function (response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json();
+    })
+    .then(timer = setTimeout(() => window.location.reload(false), 1000));
+    
+}
+
+const leaveGroup = (name,groupName) => {
+
+  var timer = null;
+  
+  var sending_data = {
+    groupName:groupName,
+  }
+
+  fetch("http://localhost:4000/api/users/"+name+"/leavegroup" , {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(sending_data)
+})
+    .then(function (response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json();
+    })
+    .then(timer = setTimeout(() => window.location.reload(false), 1200));
+    
+}
 
 const renderList = (name,listing) => {
     return (
-      listing.map((group) => {// Create if(name in group['member'])
-        return (
+      
+      listing.map((group) => {
+        var list = []
+        for(var x in group['members']){
+          list.push(group['members'][x]['name'])
+        }
+        if(list.includes(name)){
+          return (
 
             <div>
-              <li onClick={()=>onCreateGroup(name,group['groupName'])}>
+              <li onClick={()=>onChangeGroupChat(name,group['groupName'])}>
                 {group['groupName']}
-                {/* <button>Join</button> */}
+                <button onClick={()=>leaveGroup(name,group['groupName'])}>Leave</button>
               </li>
             </div>
         )
+        }
+        return (
+
+          <div>
+            <li>
+              {group['groupName']}
+              {console.log(group)}
+              <button onClick={()=>joinGroup(name,group['groupName'])}>Join</button>
+            </li>
+          </div>
+      )
+        
     })
     )
   }
@@ -33,7 +96,6 @@ const GroupList = ({ name }) => {
     const apiCall2 = await apiCall.json()
 
     var groupList = apiCall2.data;
-    console.log(groupList);
     for(var x in groupList){
       list.push(groupList[x])
     }
